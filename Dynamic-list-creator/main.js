1ac11730cf2878e696b57ed6aa23a08d7264825e
Yashika Agrawal <yashikayash2002@gmail.com>
2026-03-18 22:19:01 +0530

    Refactor main.js to improve variable naming and maintain consistency in code structure

--- Dynamic-list-creator/main.js
+++ Dynamic-list-creator/main.js
@@ -1,31 +1,32 @@
 const inputBox = document.getElementById("itemInput");
 const addBtn = document.getElementById("addBtn");
-const list = document.getElementById("list");
-addBtn.addEventListener("click", () => {
+const ul = document.getElementById("list"); //<ul></ul> -- unordered list
+addBtn.addEventListener("click", () => { 
   if (inputBox.value == "") {
     alert("Please enter a value");
     return;
   }
-
-  const li = document.createElement("li");
-  const delBtn = document.createElement("button");
+  const li = document.createElement("li"); //create a <li> element to insert in <ul>
+  const delBtn = document.createElement("button"); 
   delBtn.innerHTML = "Delete";
-  delBtn.classList.add("delete");
-  li.innerText = inputBox.value;
-  li.appendChild(delBtn);
-  list.appendChild(li);
+  delBtn.classList.add("delete"); //add delete class to delete btn
+  li.innerText = inputBox.value; //<li> will have user input value
+  li.appendChild(delBtn); //Adding delete btn to <li>
+  ul.appendChild(li); //adds new <li> to <ul>
   delBtn.addEventListener("click", () => {
-    list.removeChild(li);
+    ul.removeChild(li);
   });
   li.addEventListener("dblclick", () => {
-    const current = li.childNodes[0].textContent;
-    const edit = document.createElement("input");
-    edit.value = current;
-    li.replaceChild(edit, li.childNodes[0]); //new, old
-    edit.focus();
-    edit.addEventListener("blur", () => {
-      const backTolist = document.createTextNode(edit.value);
-      li.replaceChild(backTolist, edit); //new, old
+    console.log(li, "li") //<li> "text" <button>Delete</button/> </li>
+    console.log(li.childNodes) //NodeList(2) [text, button.delete]
+    const currentText = li.childNodes[0].textContent; //Access text of text node
+    const editInput = document.createElement("input"); //create input to edit
+    editInput.value = currentText; //Set edit input value to current list textContent
+    li.replaceChild(editInput, li.childNodes[0]); //replace textNode of list to input
+    editInput.focus();
+    editInput.addEventListener("blur", () => { //Clicking outside should save edited changes
+      const backToText = document.createTextNode(editInput.value); 
+      li.replaceChild(backToText, editInput); //replace edited input to textNode
     });
   });
   inputBox.value = "";
