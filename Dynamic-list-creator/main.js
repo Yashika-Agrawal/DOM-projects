536fecb53a27d09a32f773f292e539930cb1f354
Yashika Agrawal <yashikayash2002@gmail.com>
2026-03-18 22:02:11 +0530

    Refactor main.js for improved readability and consistency

--- Dynamic-list-creator/main.js
+++ Dynamic-list-creator/main.js
@@ -1,26 +1,32 @@
-const inputBox = document.getElementById("itemInput")
-const addBtn = document.getElementById("addBtn")
-const list = document.getElementById("list")
-addBtn.addEventListener("click", ()=>{
-    if(inputBox.value=="")
-    {
-        alert("Please enter a value")
-        return;
-    }
+const inputBox = document.getElementById("itemInput");
+const addBtn = document.getElementById("addBtn");
+const list = document.getElementById("list");
+addBtn.addEventListener("click", () => {
+  if (inputBox.value == "") {
+    alert("Please enter a value");
+    return;
+  }
 
-    console.log(inputBox.value)
-    const li=document.createElement("li")
-    const delBtn=document.createElement("button")
-    delBtn.textContent="Delete"
-    delBtn.classList.add("delete")
-    li.innerText=inputBox.value
-    li.appendChild(delBtn)
-    list.appendChild(li)
-    delBtn.addEventListener("click", ()=>{
-        list.removeChild(li)
-    })
-    inputBox.value=""
-
-
-})
-
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.innerHTML = "Delete";
+  delBtn.classList.add("delete");
+  li.innerText = inputBox.value;
+  li.appendChild(delBtn);
+  list.appendChild(li);
+  delBtn.addEventListener("click", () => {
+    list.removeChild(li);
+  });
+  li.addEventListener("dblclick", () => {
+    const current = li.childNodes[0].textContent;
+    const edit = document.createElement("input");
+    edit.value = current;
+    li.replaceChild(edit, li.childNodes[0]); //new, old
+    edit.focus();
+    edit.addEventListener("blur", () => {
+      const backTolist = document.createTextNode(edit.value);
+      li.replaceChild(backTolist, edit); //new, old
+    });
+  });
+  inputBox.value = "";
+});
